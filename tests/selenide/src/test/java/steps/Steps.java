391b15f86b98edcb684750cab9c72344529ca9c8
maksr_13 <mrozhko13@gmail.com>
2023-04-02 15:48:53 +0300

    Added server

--- tests/selenide/src/test/java/steps/Steps.java
+++ tests/selenide/src/test/java/steps/Steps.java
@@ -1,6 +1,9 @@
 package steps;
 
+import com.codeborne.selenide.Condition;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
@@ -10,5 +13,10 @@ public class Steps {
     @When("^I open web-site (.*)$")
     public void iOpenWebSite(String url) {
         open(url);
+    }
+
+    @Then("^I should see text (.*) on header$")
+    public void iShouldSeeTextXxxOnHeader(String text) {
+        $(By.cssSelector("h1")).shouldHave(visible).shouldHave(Condition.exactText(text));
     }
 }
--- tests/selenide/src/test/resources/features/test.feature
+++ tests/selenide/src/test/resources/features/test.feature
@@ -1,4 +1,5 @@
 Feature: Test
   
   Scenario: User should be able to open site
-    When I open web-site https://google.com
+    When I open web-site http://127.0.0.1:8080/index.html
+    Then I should see text Hello world on header
